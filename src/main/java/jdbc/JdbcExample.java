de92c6d62f22ebdd99018daa8dce8202c47ae55c
Lohith Kumar <lohithkumar2703@gmail.com>
2024-06-28 11:25:15 -0500

    JDBC Test Updated.

--- src/main/java/jdbc/JdbcExample.java
+++ src/main/java/jdbc/JdbcExample.java
@@ -1,10 +1,6 @@
 package jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JdbcExample {
     public static void main(String[] args) throws SQLException {
